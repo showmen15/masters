4131e2619c2f370572317a89b6eaeb80bf56f571
Michal Konarski <m4jkel@gmail.com>
2014-03-26 10:55:43 +0100

    Logging and connecting to Controller workds. Models added.

--- RobossInterface/Program.cs
+++ RobossInterface/Program.cs
@@ -2,13 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using log4net;
+using log4net.Config;
 
-namespace RobossInterface
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace RobossInterface {
+    class Program {
+        private static readonly string LOG4NET_CONFIGFILE = "log4net.config";
+        private static readonly string ROBOTNAME_VAR = "ROBOTNAME";
+
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args) {
+            if (args.Length != 3) {
+                System.Environment.SetEnvironmentVariable(ROBOTNAME_VAR, "unknown");
+                XmlConfigurator.Configure(new System.IO.FileInfo(LOG4NET_CONFIGFILE));
+
+                log.Fatal(String.Format("Invalid arguments passed: [{0}]", 
+                    string.Join(", ", args.Select(v => v.ToString()))));
+
+                return;
+            }
+
+            string hostname = args[0];
+            string port = args[1];
+            string robotName = args[2];
+
+            System.Environment.SetEnvironmentVariable(ROBOTNAME_VAR, robotName);
+            XmlConfigurator.Configure(new System.IO.FileInfo(LOG4NET_CONFIGFILE));
+
+            log.Info(String.Format("Starting with hostname = {0}, port = {1}, robotName = {2}", 
+                hostname, port, robotName));
+
+            RobotDriver robotDriver = new RobotDriver(hostname, port, robotName);
         }
     }
 }
---
+++ RobossInterface/RobotDriver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RoBOSSCommunicator;
+using log4net;
+
+namespace RobossInterface {
+
+    class RobotDriver {
+        private static readonly ILog log = LogManager.GetLogger(typeof(RobotDriver));
+
+        private string hostname;
+        private string port;
+        private string robotName;
+
+        private Communicator communicator;
+
+        public RobotDriver(string hostname, string port, string robotName) {
+
+            this.hostname = hostname;
+            this.port = port;
+            this.robotName = robotName;
+
+            this.communicator = new Communicator();
+            if (communicator.Connect(hostname, port, robotName + "_client") < 0) {
+                log.Fatal("Cannot connect to RoBOSS Controller.");
+                communicator.Dispose();
+                return;
+            }
+
+            log.Info("Connected to RoBOSS Controller.");
+
+            communicator.Dispose();
+
+        }
+    }
+}
