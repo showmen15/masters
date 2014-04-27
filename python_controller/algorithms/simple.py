b8a6665389ae6af92e0792a5b7bd8b820884adaa
Michal Konarski <m4jkel@gmail.com>
2014-04-27 16:58:13 +0200

    Avg time measuring improved

--- python_controller/algorithms/simple.py
+++ python_controller/algorithms/simple.py
@@ -4,54 +4,72 @@ import logging
 import time
 from robot_command import RobotCommand
 from robot_model import VisState
+from enum import Enum
+
+class AlgorithmState(Enum):
+    obtain_new_target = 1
+    rotate = 2
+    navigate = 3
 
 class SimpleAlgorithm:
 
     INTERVAL = 0.02 #s
+    TIME_MEASURE_STEPS = 50
 
     def __init__(self, controller, robot_name):
         self._controller = controller
         self._logger = logging.getLogger(robot_name)
         self._logger.info("Simple algorithm started")
         self._robot_name = robot_name
-        self._counter = 0
-        self._before = time.time()
-        self._start = None
+
+
         self._dir = 1
+
+        self._state = AlgorithmState.obtain_new_target
+
+        self._steps_counter = 0
+        self._period_start = None
+        self._step_start = None
+        self._step_cumulative_time = 0.0
 
     def get_robot_name(self):
         return self._robot_name
 
     def loop(self):
+        self._period_start = time.time()
+
         while True:
-            start_time = time.time()
+            self._step_start = time.time()
             states_dict = self._controller.request_states()
 
             if self._robot_name in states_dict:
                 myrobot = states_dict[self._robot_name]
 
-                if self._start is None:
-                    self._start = myrobot.get_timestamp()
-
-                self._counter += 1
-                if self._counter == 250:
-                    self._counter = 0
-                    t = (time.time() - self._before) * 4.0
-                    self._before = time.time()
-                    self._logger.info("time: %f, %d" % (t, myrobot.get_timestamp()))
-                    self._logger.info("state: %s" % (states_dict.values(), ))
-
-                if myrobot.get_timestamp() - self._start > 1000*1000*10:
-                    self._start = myrobot.get_timestamp()
-                    #self._dir *= -1
-
                 vis_state = VisState(self._robot_name, states_dict)
                 self._controller.send_vis_update(vis_state)
 
                 speed = self._dir * 5
-
                 self._controller.send_robot_command(RobotCommand(speed, speed, speed, speed))
 
-            time_diff = self.INTERVAL - (time.time() - start_time)
-            if time_diff > 0:
-                time.sleep(time_diff)
+            self._sleep_and_measure()
+
+    def _sleep_and_measure(self):
+        now = time.time()
+
+        self._steps_counter += 1
+        if self._steps_counter == self.TIME_MEASURE_STEPS:
+            avg_period = (now - self._period_start) / float(self.TIME_MEASURE_STEPS) * 1000.0
+            avg_step = self._step_cumulative_time / float(self.TIME_MEASURE_STEPS) * 1000.0
+            self._step_cumulative_time = 0.0
+            
+            self._logger.info("avg period: %f ms" % avg_period)
+            self._logger.info("avg step: %f ms" % avg_step)
+
+            self._steps_counter = 0
+            self._period_start = now
+
+        exec_time = now - self._step_start
+        self._step_cumulative_time += exec_time
+        time_diff = self.INTERVAL - exec_time
+        if time_diff > 0:
+            time.sleep(time_diff)
