870ef119f4e132e97cbee75f82deafe8c20ab6c7
Szymon <showmen15@poczta.onet.pl>
2015-02-26 15:53:03 +0100

    ograniczenie prędkości

--- python_controller/algorithms/fearful.py
+++ python_controller/algorithms/fearful.py
@@ -12,7 +12,7 @@ from utils.maze_util import MazeUtil
 from operator import itemgetter
 
 class FearfulAlgorithm(AbstractAlgorithm):
-    CRUISE_SPEED = 0.1
+    CRUISE_SPEED = 0.5
     ANGLE_MEASURE_STEPS = 5
     CIRCLES_RADIUS = 0.5
     VARIANTS_MAX_TIME = 0.015
@@ -328,8 +328,8 @@ class FearfulAlgorithm(AbstractAlgorithm):
         if abs(omega) > 0.5:
             v = 0.0
 
-        if abs(v) > FearfulAlgorithm.CRUISE_SPEED:
-	    v = math.copysign(FearfulAlgorithm.CRUISE_SPEED,v)
+        #if abs(v) > FearfulAlgorithm.CRUISE_SPEED:
+	#    v = math.copysign(FearfulAlgorithm.CRUISE_SPEED,v)
         
         return v, omega
 
